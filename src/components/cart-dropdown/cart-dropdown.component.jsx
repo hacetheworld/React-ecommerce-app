6793dee9ad8264dca0f0a14d5a681c4cbef8b3fb
Ajay <majay1638@gmail.com>
2019-12-21 10:27:40 +0530

    updated cart count code with selector library and added selector file in cart componet

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -5,11 +5,15 @@ import CartItem from '../cart-item/cart-item.component'
 import CustomButton from '../custom-button/custom-button.component';
 
 import { connect } from 'react-redux';
+
+import { selectCartItems } from '../../redux/cart/cart.selectors';
+
+
 const CartDropdown = ({ cartItems }) => (
     <div className='cart-dropdown'>
         <div className='cart-items' >{
             cartItems.map(cartItem => (<CartItem
-                key={CartItem.id}
+                key={cartItem.id}
                 item={cartItem} />))
         }
         </div>
@@ -17,7 +21,7 @@ const CartDropdown = ({ cartItems }) => (
     </div>
 );
 
-const mapStateToProps = ({ cart: { cartItems } }) => ({
-    cartItems
+const mapStateToProps = (state) => ({
+    cartItems: selectCartItems(state)
 })
 export default connect(mapStateToProps)(CartDropdown);
